5ab9166d0b719efdb8a4d9a4fd49b839d42f724f
Farooq <farooqqaisrani@outlook.com>
2018-10-10 17:33:23 +0400

    section 9 done.

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -12,7 +12,7 @@ class FullPost extends Component {
         if(this.props.id) {
             if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
 
-                axios.get('https://jsonplaceholder.typicode.com/posts/'+ this.props.id).then(response => {
+                axios.get('/posts/'+ this.props.id).then(response => {
                     this.setState({loadedPost: response.data}) ;
                     // console.log(response);
 
@@ -24,7 +24,7 @@ class FullPost extends Component {
     }
 
     deletePostHandler = () => {
-        axios.delete('https://jsonplaceholder.typicode.com/posts/'+ this.props.id).then(
+        axios.delete('/posts/'+ this.props.id).then(
             response => {
                 console.log(response);
             }
