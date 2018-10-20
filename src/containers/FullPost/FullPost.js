2276e1406225a8a8b1995b315df0efbeefb24c09
FarooqQaisrani <farooqqaisrani@outlook.com>
2018-10-20 17:21:24 +0400

    section 11 done.

--- src/containers/FullPost/FullPost.js
+++ src/containers/FullPost/FullPost.js
@@ -8,11 +8,23 @@ class FullPost extends Component {
         loadedPost : null
     }
 
-    componentDidUpdate() {
-        if(this.props.id) {
-            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+    componentDidMount() {
+        console.log(this.props);
+        this.loadData();
 
-                axios.get('/posts/'+ this.props.id).then(response => {
+
+    }
+
+    componentDidUpdate() {
+        this.loadData();
+
+    }
+
+    loadData () {
+        if(this.props.match.params.id) {
+            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id)) {
+
+                axios.get('/posts/'+ this.props.match.params.id).then(response => {
                     this.setState({loadedPost: response.data}) ;
                     // console.log(response);
 
@@ -20,11 +32,10 @@ class FullPost extends Component {
             }
 
         }
-
     }
 
     deletePostHandler = () => {
-        axios.delete('/posts/'+ this.props.id).then(
+        axios.delete('/posts/'+ this.props.match.params.id).then(
             response => {
                 console.log(response);
             }
@@ -33,7 +44,7 @@ class FullPost extends Component {
 
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
-        if (this.props.id) {
+        if (this.props.match.params.id) {
             post = <p style={{textAlign: 'center'}}>Loading....!</p>;
         }
         if(this.state.loadedPost) {
